ba50c6d8ca67c20c9c39694cfdb7cca2b1ac35cf
wangwanli <www.791832287@qq.com>
2018-05-02 11:37:17 +0800

    ceshi

--- src/views/serviceLists/components/index.js
+++
@@ -1,6 +0,0 @@
-
-export { default as LeftCollaspeContent2} from './LeftCollaspeContent2'
-
-export { default as LeftCollaspeContent1} from './LeftCollaspeContent1'
-
-export { default as RightListContent} from './RightListContent'
